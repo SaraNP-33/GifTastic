786131fa74f80465317da50d36720686915b98d5
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-03-11 22:51:35 -0700

    initial js commit- call made, image and rating divs created, started on buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,65 @@
+// start with the array of gifs
+
+var gifs = ["cat", "sloth","Sheldon","Rebel Wilson", "coding", "dolphin","unicorn"];
+console.log(gifs);
+
+//function to have the information display in the html
+
+function displayGifInfo() {
+    var gif = $ (this).attr("data-name");
+    var APIKey = "jElcC4ePU4jqFQ0rAN841qASEfZJ9BGt"
+    var queryURL = "http://api.giphy.com/v1/gifs/search?"+ APIKey+ "&q=" + gif;
+
+    // the function to call the APi through AJAX
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+
+//create a div for the incoming searched gifs
+
+        var GifDiv = $("<div class = 'gif'>");
+
+//storing the image path and data;
+
+        var imageUrl = response.data.url;
+
+        //give the url an image attribute in the html
+
+        var image = $("<img>").attr("src", imageUrl);
+
+        //append that new img attribute with the data to the new Div
+
+        GifDiv.append(image);
+
+        //repeat the same steps for the rating. Main difference we are creating a paragraph on the html
+        //not an image attr.
+
+        var rating = response.data.rating;
+        console.log(rating);
+
+        var pOne = $("<p>").text("Rating:"+ " "+ rating);
+
+        GifDiv.append(pOne);
+
+        //once all the new divs are created, prepend it on the original div created in the html
+        //the prepend will allow for the new gifs to be added to the top of the container
+        //not the bottom. 
+
+        $("#gif-view").prepend(GifDiv);
+
+      });
+};
+
+function ShowButtons(){
+    $("#button-view").empty();
+
+    for (var i=0; i<gifs.length; i++){
+
+        var newButton = $("<button class = 'gif-btn'>");
+
+        newButton.attr("data-name", gifs[i]);
+
+        newButton.text(gifs[i]);
+    }
+}
