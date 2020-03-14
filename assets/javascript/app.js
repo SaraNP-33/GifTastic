0e088f8895ff881c4b7c5796647f49ceac9a7601
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-03-14 09:29:51 -0700

    js pausing gifs working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,114 +1,116 @@
 // start with the array of gifs
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-var gifs = ["cat", "sloth","Sheldon","Rebel Wilson", "coding", "dolphin","unicorn"];
-console.log(gifs);
+    var gifs = ["cat", "sloth", "Sheldon", "Rebel Wilson", "coding", "dolphin", "unicorn"];
+    console.log(gifs);
 
-//function to have the information display in the html
+    //function to have the information display in the html
 
-function displayGifInfo() {
-    var gif = $ (this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=jElcC4ePU4jqFQ0rAN841qASEfZJ9BGt&q=" + gif;
+    function displayGifInfo() {
+        var gif = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=jElcC4ePU4jqFQ0rAN841qASEfZJ9BGt&q=" + gif;
 
-    // the function to call the APi through AJAX
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
+        // the function to call the APi through AJAX
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
-//create a div for the incoming searched gifs
-        for (var i=0; i<response.data.length; i++){
+            //create a div for the incoming searched gifs
+            for (var i = 0; i < response.data.length; i++) {
 
-       
-        var GifDiv = $("<div>");
 
-//storing the image path and data;
+                var GifDiv = $("<div>");
 
-        var imageAnim = response.data[i].images.fixed_height.url;
-        var imageStill= response.data[i].images.fixed_height_still.url;
+                //storing the image path and data;
 
-        //give the url an image attribute in the html - add the still and animated attr as well
+                var imageAnim = response.data[i].images.fixed_height.url;
+                var imageStill = response.data[i].images.fixed_height_still.url;
 
-        var image = $("<img class='gif'>");
-        image.attr("src", imageStill);
-        image.attr("data-state", "still")
-        image.attr("data-still", imageStill);
-        image.attr("data-animate", imageAnim);
-      
+                //give the url an image attribute in the html - add the still and animated attr as well
 
-        //append that new img attribute with the data to the new Div
+                var image = $("<img class='gif'>");
+                image.attr("src", imageStill);
+                image.attr("data-state", "still")
+                image.attr("data-still", imageStill);
+                image.attr("data-animate", imageAnim);
 
-        GifDiv.append(image);
 
-        //repeat the same steps for the rating. Main difference we are creating a paragraph on the html
-        //not an image attr.
+                //append that new img attribute with the data to the new Div
 
-        var rating = response.data[i].rating;
-        console.log(rating);
+                GifDiv.append(image);
 
-        var pOne = $("<p>").text("Rating:"+ " "+ rating);
+                //repeat the same steps for the rating. Main difference we are creating a paragraph on the html
+                //not an image attr.
 
-        GifDiv.append(pOne);
+                var rating = response.data[i].rating;
+                console.log(rating);
 
-        //once all the new divs are created, prepend it on the original div created in the html
-        //the prepend will allow for the new gifs to be added to the top of the container
-        //not the bottom. 
+                var pOne = $("<p>").text("Rating:" + " " + rating);
 
-        $("#gif-view").prepend(GifDiv);
+                GifDiv.append(pOne);
+
+                //once all the new divs are created, prepend it on the original div created in the html
+                //the prepend will allow for the new gifs to be added to the top of the container
+                //not the bottom. 
+
+                $("#gif-view").prepend(GifDiv);
+
+                $(".gif").on("click", function () {
+
+                    var state = $(this).attr("data-state");
+
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+
+                    }
+                    console.log("click")
+
+
+                });
+
+            }
+
+
+        });
+    };
+
+    function ShowButtons() {
+        $("#button-view").empty();
+
+        for (var i = 0; i < gifs.length; i++) {
+
+            var newButton = $("<button class = 'gif-btn'>");
+
+            newButton.attr("data-name", gifs[i]);
+
+            newButton.text(gifs[i]);
+
+            $("#button-view").append(newButton);
+
+        }
     }
 
+    $("#search-btn").on("click", function (event) {
+        event.preventDefault();
 
-      });
-};
+        var newgif = $("#search-input").val().trim();
 
-function ShowButtons(){
-    $("#button-view").empty();
+        gifs.push(newgif);
 
-    for (var i=0; i<gifs.length; i++){
+        ShowButtons();
+    })
 
-        var newButton = $("<button class = 'gif-btn'>");
+    $(document).on("click", ".gif-btn", displayGifInfo)
 
-        newButton.attr("data-name", gifs[i]);
 
-        newButton.text(gifs[i]);
 
-        $("#button-view").append(newButton);
-    
-    }
-}
-
-$("#search-btn").on("click", function(event){
-    event.preventDefault();
-
-    var newgif = $("#search-input").val().trim();
-
-    gifs.push(newgif);
 
     ShowButtons();
-})
-
-$(document).on("click", ".gif-btn", displayGifInfo)
- 
-$(Image).on("click", function(){
-
-    var state = $(this).attr("data-state");
-
-    if(state === "still"){
-   $(this).attr("src", $(this).attr("data-animate"));
-   $(this).attr("data-state", "animate");
- 
-     } else{
-   $(this).attr("src", $(this).attr("data-still"));
-   $(this).attr("data-state", "still"); 
-   
-     } 
-     console.log("click")
-
-
-});
-
-
-
-ShowButtons();
 });
